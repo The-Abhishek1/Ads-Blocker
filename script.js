cf7a9681115f0cf231ae1b7e9f3a4c1459ccefb0
Abhishek <idiot63666@gmail.com>
2022-12-21 22:09:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const detect = document.querySelector("#detect"),
-    wrapper = document.querySelector(".wrapper");
+    wrapper = document.querySelector(".wrapper"),
+    button = wrapper.querySelector("button");
 
 //these are some possible classes that adblocker avoid to render
 let adClasses = ["ad", "ads", "adsbox", "ad-placement", "doubleclick", "ad-placeholder", "ad-badge"];
@@ -8,4 +9,14 @@ for (let item of adClasses) {
 }
 //getting display property value of our detect element
 let getProperty = window.getComputedStyle(detect).getPropertyValue("display");
-console.log(getProperty);
+
+button.addEventListener("click", () => {
+    wrapper.classList.remove("show"); //hide popup on button click
+});
+
+//if wrapper doesn't contains show class the check adblocker is enabled or not
+if (!wrapper.classList.contains("show")) {
+
+    // if display property value is none then show the popup else hide it
+    getProperty == "none" ? wrapper.classList.add("show") : wrapper.classList.remove("show");
+}
